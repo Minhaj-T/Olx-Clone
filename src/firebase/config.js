828cd82d93acd1d83ec9fe4ca9fb0fb23ce6c4da
minhaj <minhajt.mh@gmail.com>
2022-05-24 20:25:21 +0530

    setup the firebase cofigutation

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPnDrbPUVcClIlW5uatHq-A-zMmeis8NM",
+    authDomain: "olx-clone-ae948.firebaseapp.com",
+    projectId: "olx-clone-ae948",
+    storageBucket: "olx-clone-ae948.appspot.com",
+    messagingSenderId: "699531156459",
+    appId: "1:699531156459:web:bf4abd6d627bf07d5157ae",
+    measurementId: "G-ZKMRYWRC5M"
+  };
+
+export const Firebase= firebase.initializeApp(firebaseConfig)
